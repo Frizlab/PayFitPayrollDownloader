e481779e84886b7e9d91208c5a4b3aaf1e0cccb2
Frizlab <fload@me.com>
2019-05-04 21:09:24 +0200

    Initial Commit

---
+++ test_payfit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_payfit
+//
+//  Created by François Lamboley on 2019/5/4.
+//  Copyright © 2019 Frizlab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
